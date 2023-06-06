152dd6d66f989b69a50a2de9b35838019d04d2b1
Jacob Moore <jmooree30@gmail.com>
2023-06-05 22:42:38 -0400

    adds hidden form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,53 +3,118 @@ import React, { Component } from "react";
 class Contact extends Component {
   constructor() {
     super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      company: "",
+      message: "",
+      website: "",
+      phone: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onClick(e) {
+    const form = document.querySelector("form");
+    if (form.checkValidity()) {
+      e.preventDefault();
+      let emailObj = {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        company: this.state.company,
+        message: this.state.message,
+        website: this.state.website,
+        phone: this.state.phone
+      };
+      document.getElementById("realFormName").value = "contact";
+      document.getElementById("firstName").value = this.state.firstName;
+      document.getElementById("lastName").value = this.state.lastName;
+      document.getElementById("email").value = this.state.email;
+      document.getElementById("company").value = this.state.company;
+      document.getElementById("message").value = this.state.message;
+      document.getElementById("website").value = this.state.website;
+      document.getElementById("phone").value = this.state.phone;
+      document.getElementById("myForm").submit();
+    }
   }
   render() {
     return (
       <div>
-        <div id="fifty"></div>
-        <div class="contact-container" id="max">
-          <form name="contact" method="POST" netlify="true">
-            <div class="input-container">
+        <div id="fifty" />
+        <div className="contact-container" id="max">
+          <form>
+            <div className="input-container">
               <label>First Name</label>
               <br />
-              <input type="text" name="firstName" required />
+              <input
+                onChange={this.onChange}
+                type="text"
+                name="firstName"
+                required
+              />
             </div>
-            <div class="input-container">
+            <div className="input-container">
               <label>Last Name</label>
               <br />
-              <input type="text" name="lastName" required />
+              <input
+                onChange={this.onChange}
+                type="text"
+                name="lastName"
+                required
+              />
             </div>
-            <div class="input-container">
+            <div className="input-container">
               <label>Phone Number</label>
               <br />
-              <input type="tel" name="phone" />
+              <input onChange={this.onChange} type="tel" name="phone" />
             </div>
-            <div class="input-container">
+            <div className="input-container">
               <label>Website</label>
               <br />
-              <input type="text" name="website" />
+              <input onChange={this.onChange} type="text" name="website" />
             </div>
-            <div class="input-container">
+            <div className="input-container">
               <label>Company</label>
               <br />
-              <input type="text" name="company" />
+              <input onChange={this.onChange} type="text" name="company" />
             </div>
-            <div class="input-container">
+            <div className="input-container">
               <label>Email</label>
-              <input type="text" name="email" required />
+              <input
+                onChange={this.onChange}
+                type="text"
+                name="email"
+                required
+              />
             </div>
-            <div class="textarea-container">
+            <div className="textarea-container">
               <label>Message</label>
-              <textarea rows="5" name="message" cols="30" required></textarea>
+              <textarea
+                onChange={this.onChange}
+                rows={5}
+                name="message"
+                cols={30}
+                required
+              />
             </div>
-            <button class="btn btn-success" type="submit">
-              &nbsp;&nbsp;&nbsp;<i class="fa fa-envelope">&nbsp;&nbsp;</i>
+            <button
+              className="btn btn-success"
+              type="submit"
+              name="submit"
+              onClick={this.onClick}
+            >
+              &nbsp;&nbsp;&nbsp;
+              <i className="fa fa-envelope">&nbsp;&nbsp;</i>
             </button>
           </form>
         </div>
       </div>
-
     );
   }
 }
