069854bf66ab47adc30068367b2a98e907819574
Jacob Moore <jmooree30@gmail.com>
2023-06-09 01:27:33 -0400

    coverting to functional component - Comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,33 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/comments.css";
 
-class Comments extends Component {
-  constructor() {
-    super();
-    this.state = {
-      comments: [],
-      name: "",
-      comment: ""
-    };
-  }
+const Comments = ({ type }) => {
+  const [comments, setComments] = useState([]);
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
 
-  componentDidMount() {
-    this.getComments(this.props.type);
-  }
+  useEffect(() => {
+    getComments(type);
+  }, [type]);
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = e => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "comment") {
+      setComment(value);
+    }
   };
 
-  onClick = e => {
-    let commentObj = {
-      comment: this.state.comment,
-      name: this.state.name,
-      blog: this.props.type
+  const onClick = e => {
+    e.preventDefault();
+    const commentObj = {
+      comment: comment,
+      name: name,
+      blog: type
     };
-    const url = process.env.NODE_ENV === 'production'
-      ? 'https://jacobmoore.dev/.netlify/functions/add-comment'
-      : 'http://localhost:3000/.netlify/functions/add-comment';
+    const url =
+      process.env.NODE_ENV === "production"
+        ? "https://jacobmoore.dev/.netlify/functions/add-comment"
+        : "http://localhost:3000/.netlify/functions/add-comment";
 
     fetch(url, {
       method: "POST",
@@ -38,100 +40,91 @@ class Comments extends Component {
     })
       .then(res => res.json())
       .then(response => {
-        this.setState({
-          comments: [...this.state.comments, response]
-        });
+        setComments([...comments, response]);
       })
       .catch(error => console.error("Error:", error));
   };
 
-  getComments = type => {
-    const url = process.env.NODE_ENV === 'production'
-      ? 'https://jacobmoore.dev/.netlify/functions/get-comments'
-      : 'http://localhost:3000/.netlify/functions/get-comments';
+  const getComments = type => {
+    const url =
+      process.env.NODE_ENV === "production"
+        ? "https://jacobmoore.dev/.netlify/functions/get-comments"
+        : "http://localhost:3000/.netlify/functions/get-comments";
 
     fetch(url)
       .then(res => res.json())
       .then(comments => {
-        let commentsArr = [];
-        comments.forEach(e => {
-          if (e.blog === type) {
-            commentsArr.push(e);
-          }
-        });
-        this.setState({ comments: commentsArr.reverse() });
+        const commentsArr = comments.filter(e => e.blog === type).reverse();
+        setComments(commentsArr);
       });
   };
 
-
-  render() {
-    let comments = this.state.comments;
-    return (
-      <div>
-        <div className="row bootstrap snippets">
-          <div className="comments-margin">
-            <div className="comment-wrapper">
-              <div className="panel panel-info">
-                <div className="panel-heading">Comments</div>
-                <div className="panel-body">
-                  <input
-                    placeholder="Name"
-                    name="name"
-                    className="form-control"
-                    onChange={this.onChange}
-                  />
-                  <br />
-                  <textarea
-                    className="form-control"
-                    placeholder="write a comment..."
-                    name="comment"
-                    onChange={this.onChange}
-                    rows={3}
-                    defaultValue={""}
-                    required
-                  />
-                  <br />
-                  <button
-                    type="submit"
-                    className="btn btn-info pull-right"
-                    onClick={this.onClick}
-                  >
-                    Post
-                  </button>
-                  <div className="clearfix" />
-                  <hr />
-                  <ul className="media-list">
-                    {comments.map(function (object, i) {
-                      return (
-                        <li className="media">
-                          <div className="media-body">
-                            <span className="text-muted pull-right">
-                              <small className="text-muted">
-                                {object.date.substring(0, 10)}
-                              </small>
-                            </span>
-                            <strong
-                              className="text underline"
-                              style={{ float: "left" }}
-                            >
-                              {object.name}
-                            </strong>
-                            <br />
-                            <p style={{ fontSize: "20px", float: "left" }}>
-                              {object.text}
-                            </p>
-                          </div>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
+  return (
+    <div>
+      <div className="row bootstrap snippets">
+        <div className="comments-margin">
+          <div className="comment-wrapper">
+            <div className="panel panel-info">
+              <div className="panel-heading">Comments</div>
+              <div className="panel-body">
+                <input
+                  placeholder="Name"
+                  name="name"
+                  className="form-control"
+                  onChange={onChange}
+                />
+                <br />
+                <textarea
+                  className="form-control"
+                  placeholder="write a comment..."
+                  name="comment"
+                  onChange={onChange}
+                  rows={3}
+                  defaultValue={""}
+                  required
+                />
+                <br />
+                <button
+                  type="submit"
+                  className="btn btn-info pull-right"
+                  onClick={onClick}
+                >
+                  Post
+                </button>
+                <div className="clearfix" />
+                <hr />
+                <ul className="media-list">
+                  {comments.map(function (object, i) {
+                    return (
+                      <li className="media" key={i}>
+                        <div className="media-body">
+                          <span className="text-muted pull-right">
+                            <small className="text-muted">
+                              {object.date.substring(0, 10)}
+                            </small>
+                          </span>
+                          <strong
+                            className="text underline"
+                            style={{ float: "left" }}
+                          >
+                            {object.name}
+                          </strong>
+                          <br />
+                          <p style={{ fontSize: "20px", float: "left" }}>
+                            {object.text}
+                          </p>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default Comments;
