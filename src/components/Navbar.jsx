98b877d5f8cf9058798a39dafaa3acf63359df67
Jacob Moore <jmooree30@gmail.com>
2023-06-09 01:33:55 -0400

    converting to functional components - Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,86 +1,88 @@
-import React, { Component } from "react";
-import resume from "../assets/resume.pdf";
+import React, { useState, useEffect } from "react";
 import { HashLink as Link } from "react-router-hash-link";
+import resume from "../assets/resume.pdf";
 
-export default class Navbar extends Component {
-  constructor() {
-    super();
-    this.state = {
-      scrolled: false
-    };
-  }
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
 
-  componentDidMount() {
+  useEffect(() => {
     const root = "https://jacobmoore.dev/";
 
     if (process.env.NODE_ENV !== "production") {
-      this.setState({ scrolled: true });
+      setScrolled(true);
     }
 
-    window.addEventListener("scroll", () => {
+    const handleScroll = () => {
       if (window.location.href === root) {
-        this.setState({ scrolled: true });
+        setScrolled(true);
       }
-    });
+    };
+
+    window.addEventListener("scroll", handleScroll);
 
     if (window.location.href !== root) {
-      this.setState({ scrolled: true });
+      setScrolled(true);
     }
-  }
 
-  render() {
-    return (
-      <nav
-        className="navbar navbar-default navbar-fixed-top"
-        style={{ top: this.state.scrolled ? 0 : -75 }}
-      >
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle"
-              data-toggle="collapse"
-              data-target="#myNavbar"
-            >
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <Link to={"/#landing-top"}>
-              <a className="navbar-brand">JACOB MOORE</a>
-            </Link>
-          </div>
-          <div className="collapse navbar-collapse" id="myNavbar">
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a
-                  href="https://github.com/jmooree30"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="navv"
-                >
-                  GITHUB
-                </a>
-              </li>
-              <li>
-                <a href={resume} target="_blank" className="navv">
-                  RESUME
-                </a>
-              </li>
-              <li>
-                <Link to={"/#fifty"} className="navv">
-                  CONTACT
-                </Link>
-              </li>
-              <li>
-                <Link to={"/blog"} className="navv">
-                  BLOG
-                </Link>
-              </li>
-            </ul>
-          </div>
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`navbar navbar-default navbar-fixed-top ${
+        scrolled ? "scrolled" : ""
+        }`}
+    >
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target="#myNavbar"
+          >
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <Link to={"/#landing-top"}>
+            <a className="navbar-brand">JACOB MOORE</a>
+          </Link>
         </div>
-      </nav>
-    );
-  }
-}
+        <div className="collapse navbar-collapse" id="myNavbar">
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a
+                href="https://github.com/jmooree30"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="navv"
+              >
+                GITHUB
+              </a>
+            </li>
+            <li>
+              <a href={resume} target="_blank" className="navv">
+                RESUME
+              </a>
+            </li>
+            <li>
+              <Link to={"/#fifty"} className="navv">
+                CONTACT
+              </Link>
+            </li>
+            <li>
+              <Link to={"/blog"} className="navv">
+                BLOG
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
